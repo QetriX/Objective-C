08ccee1596d4f2671de9b6f66fe20daa64a181c2
mikiqex <michal.novak@qex.cz>
2015-12-15 12:46:24 +0100

    Create util.h

---
+++ libs/util.h
@@ -0,0 +1,15 @@
+//
+//  Util.h
+//
+//  Created by QetriX on 25/04/14.
+//  Copyright (c) 2015 QetriX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Util : NSObject
+
+- (NSString *)UrlEncode:(NSString *)value;
+- (NSString *)UrlDecode:(NSString *)value;
+
+@end 
